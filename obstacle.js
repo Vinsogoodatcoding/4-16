493b3e02265c22991864d2f130421fa51509ad84
Vinsogoodatcoding <holupwaitwhat123@Gmail.com>
2026-04-23 08:29:54 -1000

    where da box go

--- obstacle.js
+++ obstacle.js
@@ -1,20 +1,16 @@
 //obstacle object
 function obstacle() {
-    this.x = window.innerWidth
-    this.y = 100
+    //start location
+    this.x = -400
+    this.y = -400
 
     this.show = function() {
         fill(0,0,255)
-        rect(this.x, this.y, 400, 400)
+        rect(this.x,this.y,400,400)
     }
-    this.update = function() {
-        this.x -= player.speed
-    }
-}
-
 //This obstacle needs to start off the screen to the right
 //It should move to the left
 //Once it is no longer visible, place it back on the right hand side off the screen
 //prob has sum to do with framerate()
-
+}
 
