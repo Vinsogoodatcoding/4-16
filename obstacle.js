15dede4d316ec17231df8737d0c4175ec41469fd
Vinsogoodatcoding <holupwaitwhat123@Gmail.com>
2026-04-23 08:27:27 -1000

    wtf

--- obstacle.js
+++ obstacle.js
@@ -1,16 +1,20 @@
 //obstacle object
 function obstacle() {
-    //start location
-    this.x = -400
-    this.y = -400
+    this.x = window.innerWidth
+    this.y = 100
 
     this.show = function() {
         fill(0,0,255)
-        rect(this.x,this.y,400,400)
+        rect(this.x, this.y, 400, 400)
     }
+    this.update = function() {
+        this.x -= player.speed
+    }
+}
+
 //This obstacle needs to start off the screen to the right
 //It should move to the left
 //Once it is no longer visible, place it back on the right hand side off the screen
 //prob has sum to do with framerate()
-}
+
 
